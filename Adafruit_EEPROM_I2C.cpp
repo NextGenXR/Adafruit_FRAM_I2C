b4bf9509912b9b9c43068680747777e16ad6cc28
NextGenXR <jenni@nextgenxr.com>
2022-12-04 12:55:36 -0600

    Standard formatting

--- Adafruit_EEPROM_I2C.cpp
+++ Adafruit_EEPROM_I2C.cpp
@@ -10,7 +10,9 @@
 
 #include <stm32yyxx_hal_i2c.h>
 
-extern "C" I2C_HandleTypeDef hi2c1;
+// TODO: For CM3K not likely to be on I2C1.
+// Only used if hi2c handle isn't provided
+extern "C" I2C_HandleTypeDef hi2c2;
 
 /*========================================================================*/
 /*                            CONSTRUCTORS                                */
@@ -18,10 +20,13 @@ extern "C" I2C_HandleTypeDef hi2c1;
 
 /**************************************************************************/
 /*!
-    Constructor
-*/
+ Constructor
+ */
 /**************************************************************************/
-Adafruit_EEPROM_I2C::Adafruit_EEPROM_I2C(void) {}
+Adafruit_EEPROM_I2C::Adafruit_EEPROM_I2C(void) {
+
+	hi2c = &hi2c2;	// Assign the default I2C, if needed
+}
 
 /*========================================================================*/
 /*                           PUBLIC FUNCTIONS                             */
@@ -37,109 +42,114 @@ Adafruit_EEPROM_I2C::Adafruit_EEPROM_I2C(void) {}
  */
 bool Adafruit_EEPROM_I2C::begin(uint8_t addr, I2C_HandleTypeDef *handle) {
 
-	if(handle == NULL)
-	{
-		if(hi2c1.State == (HAL_I2C_StateTypeDef::HAL_I2C_STATE_READY))
-			i2c_dev = new Adafruit_I2CDevice(addr, &hi2c1);
-	}
-	else
-	{
-		  i2c_dev = new Adafruit_I2CDevice(addr, handle);
+	// Is the Adafruit I2C Device already provided ?
+	if (handle == NULL) {
+		// Check to see if the extern I2C is ready
+		// TODO: NULL check on hi2c1 to see if it needs to be configured
+		if (hi2c->State == (HAL_I2C_StateTypeDef::HAL_I2C_STATE_READY)) {
+			i2c_dev = new Adafruit_I2CDevice(addr, hi2c);
+		} else {
+			// TODO: Configure the default I2C device?
+			// Throw error, or wait?
+		}
+	} else {
+		hi2c = handle;
+		i2c_dev = new Adafruit_I2CDevice(addr, hi2c);
 	}
 
-  _addr = addr;
+	_addr = addr;
 
-  return i2c_dev->begin();
+	return i2c_dev->begin();
 }
 
 /**************************************************************************/
 /*!
-    @brief  Writes a byte at the specific EEPROM address
+ @brief  Writes a byte at the specific EEPROM address
 
-    @param[in] addr
-                The 16-bit address to write to in EEPROM memory
-    @param[in] value
-                The 8-bit value to write at addr
-    @returns True on I2C command success, false on timeout or I2C failure
-*/
+ @param[in] addr
+ The 16-bit address to write to in EEPROM memory
+ @param[in] value
+ The 8-bit value to write at addr
+ @returns True on I2C command success, false on timeout or I2C failure
+ */
 /**************************************************************************/
 bool Adafruit_EEPROM_I2C::write(uint16_t addr, uint8_t value) {
-  uint8_t buff[3] = {(uint8_t)(addr >> 8), (uint8_t)addr, value};
+	uint8_t buff[3] = { (uint8_t) (addr >> 8), (uint8_t) addr, value };
 
-  if (!i2c_dev->write(buff, 3))
-    return false;
+	if (!i2c_dev->write(buff, 3))
+		return false;
 
-  // Wait until it acks!
-  uint8_t timeout = 100;
-  while (timeout--) {
-    if (i2c_dev->detected())
-      return true;
-    delay(1);
-  }
+	// Wait until it acks!
+	uint8_t timeout = 100;
+	while (timeout--) {
+		if (i2c_dev->detected())
+			return true;
+		delay(1);
+	}
 
-  // timed out :(
-  return false;
+	// timed out :(
+	return false;
 }
 
 /**************************************************************************/
 /*!
-    @brief  Reads an 8 bit value from the specified EEPROM address
-    @param addr
-                The 16-bit address to read from in EEPROM memory
-    @returns    The 8-bit value retrieved at addr
-*/
+ @brief  Reads an 8 bit value from the specified EEPROM address
+ @param addr
+ The 16-bit address to read from in EEPROM memory
+ @returns    The 8-bit value retrieved at addr
+ */
 /**************************************************************************/
 uint8_t Adafruit_EEPROM_I2C::read(uint16_t addr) {
-  uint8_t buff[2] = {(uint8_t)(addr >> 8), (uint8_t)addr};
+	uint8_t buff[2] = { (uint8_t) (addr >> 8), (uint8_t) addr };
 
-  if (!i2c_dev->write_then_read(buff, 2, buff, 1))
-    return 0x0;
+	if (!i2c_dev->write_then_read(buff, 2, buff, 1))
+		return 0x0;
 
-  return buff[0];
+	return buff[0];
 }
 
 /**************************************************************************/
 /*!
-    @brief  Writes multiple bytes at the specific EEPROM address
+ @brief  Writes multiple bytes at the specific EEPROM address
 
-    @param[in] addr
-                The 16-bit address to write to in EEPROM memory
-    @param[in] buffer Pointer to buffer of bytes to write
-    @param num How many bytes to write!
-    @returns True on I2C command success, false on timeout or I2C failure
-*/
+ @param[in] addr
+ The 16-bit address to write to in EEPROM memory
+ @param[in] buffer Pointer to buffer of bytes to write
+ @param num How many bytes to write!
+ @returns True on I2C command success, false on timeout or I2C failure
+ */
 /**************************************************************************/
 bool Adafruit_EEPROM_I2C::write(uint16_t addr, uint8_t *buffer, uint16_t num) {
-  while (num--) {
-    if (!write(addr++, buffer[0])) {
-      return false;
-    }
-    buffer++;
-  }
-  return true;
+	while (num--) {
+		if (!write(addr++, buffer[0])) {
+			return false;
+		}
+		buffer++;
+	}
+	return true;
 }
 
 /**************************************************************************/
 /*!
-    @brief  Reads multiple bytes from the specified EEPROM address
-    @param addr
-                The 16-bit address to read from in EEPROM memory
-    @param buffer Pointer to buffer of bytes that will be filled!
-    @param num How many bytes to write!
-    @returns    The 8-bit value retrieved at addr
-*/
+ @brief  Reads multiple bytes from the specified EEPROM address
+ @param addr
+ The 16-bit address to read from in EEPROM memory
+ @param buffer Pointer to buffer of bytes that will be filled!
+ @param num How many bytes to write!
+ @returns    The 8-bit value retrieved at addr
+ */
 /**************************************************************************/
 bool Adafruit_EEPROM_I2C::read(uint16_t addr, uint8_t *buffer, uint16_t num) {
 
-  for (uint16_t i = 0; i < num; i++) {
-    uint8_t buff[2] = {(uint8_t)(addr >> 8), (uint8_t)addr};
+	for (uint16_t i = 0; i < num; i++) {
+		uint8_t buff[2] = { (uint8_t) (addr >> 8), (uint8_t) addr };
 
-    if (!i2c_dev->write_then_read(buff, 2, buff, 1))
-      return false;
-    buffer[i] = buff[0];
+		if (!i2c_dev->write_then_read(buff, 2, buff, 1))
+			return false;
+		buffer[i] = buff[0];
 
-    addr++;
-  }
+		addr++;
+	}
 
-  return true;
+	return true;
 }
